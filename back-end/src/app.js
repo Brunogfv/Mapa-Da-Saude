8e5100f60806bc06bb48402cf0fcd630b83f647a
Brunogfv <bruno.gfv@gmail.com>
2026-01-13 17:41:38 -0300

    Inicio do back-end

---
+++ back-end/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json);
+
+// Rotas
+const especialidadesRoutes = require("./routes/especialidades.routes");
+const medicosRoutes = require("./routes/medicos.routes");
+
+app.use("/especialidades", especialidadesRoutes);
+app.use("/medicos", medicosRoutes);
+
+module.exports = app;
